e3b3fc097bc014c73e7e1da3bc3627f058a3c6a7
polako <polako.rafa@gmail.com>
2019-10-15 17:34:09 -0300

    first commit

---
+++ data.js
@@ -0,0 +1,25 @@
+window.audios = [{
+        title: "Simple Man",
+        artist: "Lynyrd Skynyrd ",
+        cover: "LYNYERD.jpg",
+        file: "simple-man-lynyrd-skynyrd-lyrics-hd.mp3"
+    },
+    {
+        title: "Sweet Home Alabama",
+        artist: "Lynyrd Skynyrd",
+        cover: "SWEET_HOME.jpg",
+        file: "lynyrd-skynyrd-sweet-home-alabama.mp3"
+    },
+    {
+        title: "Better man",
+        artist: "Pearl jam",
+        cover: "Pearl-Jam-OFFICIAL.jpg",
+        file: "pearl-jam-better-man.mp3"
+    },
+    {
+        title: "AC/DC ",
+        artist: "Back In Black ",
+        cover: "AC_DC.jpg",
+        file: "acdc-back-in-black-official-video.mp3"
+    }
+];
