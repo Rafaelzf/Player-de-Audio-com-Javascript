646fe0cb196c3631a30343974dd1b1976c02517c
Rafael Zanini Francucci <53485909+Rafaelzf@users.noreply.github.com>
2019-10-15 18:12:31 -0300

    Add files via upload

--- data.js
+++ data.js
@@ -1,15 +1,17 @@
 window.audios = [{
-        title: "Simple Man",
-        artist: "Lynyrd Skynyrd ",
-        cover: "LYNYERD.jpg",
-        file: "simple-man-lynyrd-skynyrd-lyrics-hd.mp3"
-    },
-    {
         title: "Sweet Home Alabama",
         artist: "Lynyrd Skynyrd",
         cover: "SWEET_HOME.jpg",
         file: "lynyrd-skynyrd-sweet-home-alabama.mp3"
     },
+
+    {
+        title: "Simple Man",
+        artist: "Lynyrd Skynyrd ",
+        cover: "LYNYERD.jpg",
+        file: "simple-man-lynyrd-skynyrd-lyrics-hd.mp3"
+    },
+
     {
         title: "Better man",
         artist: "Pearl jam",
